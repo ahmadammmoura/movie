c81fd6e07402d4e933edb1a67d45a6f3e412559c
ahmadammmoura <ammoura575@gmail.com>
2021-10-13 14:08:27 +0300

    alert

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import Nav from './components/Nav';
 
 import MovieProfile from './components/MovieProfile';
 import { Switch, Route } from "react-router-dom";
+import { useEffect } from 'react';
 
 function App() {
+
+
+
+  useEffect(()=>{
+    alert('all the functionality are working fine but i worked on the project for hour and half cuz my laptob always surbrise me ')
+
+  },[])
   return (
     <div className="App">
       <Switch>
